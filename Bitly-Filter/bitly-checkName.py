5cedb8377173aa65e8bbc3754e525899f7192060
prathmesh-Chaudhari05 <prathmeshchaudhari05@gmail.com>
2022-03-14 17:01:09 +0530

    Update to Fetch Title Name

--- Bitly-Filter/bitly-checkName.py
+++ Bitly-Filter/bitly-checkName.py
@@ -1,17 +1,22 @@
+from urllib import response
+from matplotlib.pyplot import title
 from selenium import webdriver
 import json, time
+from urllib.request import urlopen
+
 
 f1 = open(r"C:\Users\pc\Desktop\pythonPrathmesh\Project-List\Bitly-Filter\Description.txt", mode='r+')
 # print(f1.read())
 
 driver = webdriver.Chrome()
-
+newSym = "+"
 for i in f1:
-    newLink = i+"+"
-    driver.maximize_window()  
+    print(i)
+    newLink = i + newSym
+    # driver.maximize_window()  
     driver.get(newLink)
-
-    # print(f1.readline(1))
+    title = driver.find_element_by_class_name("item-detail--title")
+    print(title)
     time.sleep(3)
 
-driver.close()
+# driver.close()
